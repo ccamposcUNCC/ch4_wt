e8fc194ce845cac9380a2a50420818c38c967cd9
Carlos Campos <ccamposc@uncc.edu>
2016-10-12 05:25:22 +0000

    Done with section 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,16 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.html.haml by default
   end
   
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end
+  
+  # add below all other methods
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  end
+  
 end
